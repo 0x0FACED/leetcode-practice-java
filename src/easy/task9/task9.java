6d03efae14c25b41a7f502482813ea34467e7de7
NyNemnogoGnida <102946972+MisterGnida@users.noreply.github.com>
2023-03-13 21:41:41 +0300

    added task9

---
+++ src/easy/task9/task9.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x < 0 || (x % 10 == 0 && x != 0)){
+            return false;
+        }
+        String value = String.valueOf(x);
+        int len = value.length();
+        for(int i = 0; i < len / 2; ++i){
+            if(value.charAt(i) != value.charAt(len - 1 - i)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
